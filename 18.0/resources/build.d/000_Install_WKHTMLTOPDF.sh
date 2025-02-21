58b213ece30e25e44dd932db98c9d4c2a50f0d61
Andrés Zacchino <az@adhoc.com.ar>
2025-02-21 12:30:18 -0300

    [FIX] files permissions

--- 18.0/resources/build.d/000_Install_WKHTMLTOPDF.sh
+++ 18.0/resources/build.d/000_Install_WKHTMLTOPDF.sh
@@ -10,8 +10,8 @@ fi
 
 curl -sLo libjpeg-turbo8.deb http://mirrors.kernel.org/ubuntu/pool/main/libj/libjpeg-turbo/libjpeg-turbo8_2.1.2-0ubuntu1_amd64.deb
 curl -sLo wkhtmltox.deb https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
-apt-get install -yqq --no-install-recommends
-    ./libjpeg-turbo8.deb
+apt-get install -yqq --no-install-recommends \
+    ./libjpeg-turbo8.deb \
     ./wkhtmltox.deb
 rm -Rf wkhtmltox.deb libjpeg-turbo8.deb
 
